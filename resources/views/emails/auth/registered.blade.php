e149edfedde5eb67e4aa8d6b0e5115a72886b8b5
Guillaume Wells <wellsguillaume@gmail.com>
2020-09-02 21:32:20 +0200

    supress

--- resources/views/emails/auth/registered.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-# Email validation
-
-Please follow this link to validate your [registration](http://validate-email)
-
-Thanks,<br>
-
-{{ config('app.name') }}
-@endcomponent
